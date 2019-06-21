de50cb3855c1c69a9a48838b466f4d38b840dc3c
Roberson <robersonupc@gmail.com>
2019-06-21 15:30:47 -0300

    Testando a primeira class de domain

--- src/main/java/com/rds/cursomc/resources/CategoryResource.java
+++ src/main/java/com/rds/cursomc/resources/CategoryResource.java
@@ -1,16 +1,33 @@
 package com.rds.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rds.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Servir");
+		Category cat2 = new Category(2, "Freezer");
+		Category cat3 = new Category(3, "Refrigerador");
+		Category cat4 = new Category(4, "Micro-ondas");
+		
+		List<Category> lista = new ArrayList<Category>();
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		lista.add(cat4);
+		
+		return lista;
 	}
 
 }
