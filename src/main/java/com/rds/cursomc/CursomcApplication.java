1b6a2dfd25a9384f5aaeaefaf7c4c074984faef7
Roberson <robersonupc@gmail.com>
2019-06-22 07:37:54 -0300

    Criando operação de instanciação

--- src/main/java/com/rds/cursomc/CursomcApplication.java
+++ src/main/java/com/rds/cursomc/CursomcApplication.java
@@ -1,13 +1,37 @@
 package com.rds.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.rds.cursomc.domain.Category;
+import com.rds.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Servir");
+		Category cat2 = new Category(null, "Midro-ondas");
+		Category cat3 = new Category(null, "Refregerador");
+		Category cat4 = new Category(null, "Freezer");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+		
+	}
+	
+	
 
 }
