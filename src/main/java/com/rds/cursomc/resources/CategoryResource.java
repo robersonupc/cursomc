29783ed9af761c2f05e2852062b683ff9a700c9a
Roberson <robersonupc@gmail.com>
2019-06-24 23:29:18 -0300

    Listando todas as categories com DTO

--- src/main/java/com/rds/cursomc/resources/CategoryResource.java
+++ src/main/java/com/rds/cursomc/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.rds.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rds.cursomc.domain.Category;
+import com.rds.cursomc.dto.CategoryDTO;
 import com.rds.cursomc.services.CategoryService;
 
 @RestController
@@ -49,6 +52,13 @@ public class CategoryResource {
 		service.delete(id);		
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/rds/cursomc/services/CategoryService.java
+++ src/main/java/com/rds/cursomc/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.rds.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +49,10 @@ public class CategoryService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegratyException("Não é possível excluir uma categoria que possui produtos!");
 		}
+	}
+	
+	public List<Category> findAll(){
+		return repo.findAll();
 	}
 	
 }
