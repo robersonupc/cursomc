911b8d50ca1d222dbf1521ab8fa6d94e1cd70063
Roberson <robersonupc@gmail.com>
2019-07-01 07:31:54 -0300

    Email HTML

--- src/main/java/com/rds/cursomc/services/EmailService.java
+++ src/main/java/com/rds/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.rds.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.rds.cursomc.domain.Pedido;
@@ -9,6 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 	
 	//void sendNewPasswordEmail(Client client, String newPass);
 }
--- src/main/java/com/rds/cursomc/services/MockEmailService.java
+++ src/main/java/com/rds/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.rds.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -11,6 +13,14 @@ public class MockEmailService extends AbstractEmailService {
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de E-mail...");
+		LOG.info(msg.toString());
+		LOG.info("E-mail enviado com sucesso!");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de E-mail HTML...");
 		LOG.info(msg.toString());
 		LOG.info("E-mail enviado com sucesso!");
 		
