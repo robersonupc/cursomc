10c144864597f0da976f444680c21d80df5a637d
Roberson <robersonupc@gmail.com>
2019-07-05 00:04:02 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/rds/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/rds/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.rds.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rds.cursomc.domain.Client;
 import com.rds.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly = true)
+	Page<Pedido> findByClient(Client client, Pageable pageRequest);
 
 }
--- src/main/java/com/rds/cursomc/services/PedidoService.java
+++ src/main/java/com/rds/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.rds.cursomc.domain.Client;
 import com.rds.cursomc.domain.ItemPedido;
 import com.rds.cursomc.domain.PaymentCombotion;
 import com.rds.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.rds.cursomc.domain.enums.StatePayment;
 import com.rds.cursomc.repositories.ItemPedidoRepository;
 import com.rds.cursomc.repositories.PaymentRepository;
 import com.rds.cursomc.repositories.PedidoRepository;
+import com.rds.cursomc.security.UserSS;
+import com.rds.cursomc.services.exceptions.AuthorizationException;
 import com.rds.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,5 +76,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Client client =  clientService.find(user.getId());
+		return repo.findByClient(client, pageRequest);
 	}
 }
